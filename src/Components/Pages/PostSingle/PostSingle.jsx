83f782396cd7c0a7c3b956f71a7744d0d4344ba6
sardornn <sardornoraliev01@gmail.com>
2022-06-20 18:35:55 +0500

    xatolik topildi

--- src/Components/Pages/PostSingle/PostSingle.jsx
+++ src/Components/Pages/PostSingle/PostSingle.jsx
@@ -4,23 +4,24 @@ import './PostSingle.scss'
 export const PostSingle = () =>{
     const [postSingle, setPostSingle] = useState([])
     const params = useParams();
-  const navigate = useNavigate();
-
+    const navigate = useNavigate();
+    
     useEffect ( ()=>{
         fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
         .then((res) => res.json())
         .then((data) => setPostSingle(data))
     }, [params])
-  useEffect(()=> { if(isNaN(params.postId)){
-    return navigate(-1)
-}},[])
+    useEffect(()=> { if(isNaN(params.postId)){
+        return navigate(-1)
+    }})
     return(
         <>
         <div className='postsingle'>
-            <h2>{postSingle.title}</h2>
-            <p>{postSingle.body}</p>
+        <h2>{postSingle.title}</h2>
+        <p>{postSingle.body}</p>
         </div>
-      
+        
         </>
-    )
-}
+        )
+    }
+    
